1fcdc8b47dd3b7b7d777134d056a860e71920bf1
Liangjing Shao <91866296+BaymaxShao@users.noreply.github.com>
2025-03-18 12:06:39 +0800

    Add files via upload

---
+++ models/jfe.py
@@ -0,0 +1,237 @@
+from functools import partial
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from timm.models.layers import trunc_normal_, DropPath
+from timm.models.registry import register_model
+from timm.data import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
+from einops import rearrange
+import math
+
+
+class MD(nn.Module):
+    def __init__(self, k_size, pool_types=['avg', 'max']):
+        super(MD, self).__init__()
+
+        self.pools = nn.ModuleList([])
+        for pool_type in pool_types:
+            if pool_type == 'avg':
+                self.pools.append(nn.AdaptiveAvgPool2d(1))
+            elif pool_type == 'max':
+                self.pools.append(nn.AdaptiveMaxPool2d(1))
+            else:
+                raise NotImplementedError
+
+        self.conv = nn.Conv2d(1, 1, kernel_size=(1, k_size), stride=1, padding=(0, (k_size - 1) // 2), bias=False).cuda()
+        self.sigmoid = nn.Sigmoid()
+
+        self.weight = nn.Parameter(torch.rand(2))
+
+    def forward(self, x):
+        feats = [pool(x) for pool in self.pools]
+
+        if len(feats) == 1:
+            out = feats[0]
+        elif len(feats) == 2:
+            weight = torch.sigmoid(self.weight)
+            out = 1 / 2 * (feats[0] + feats[1]) + weight[0] * feats[0] + weight[1] * feats[1]
+        else:
+            assert False, "Feature Extraction Exception!"
+
+        out = out.permute(0, 3, 2, 1).contiguous()
+        out = self.conv(out)
+        out = out.permute(0, 3, 2, 1).contiguous()
+
+        out = self.sigmoid(out)
+        out = out.expand_as(x)
+
+        return x * out
+
+
+class MDLayer(nn.Module):
+    def __init__(self, inp, no_spatial=False):
+        super(MDLayer, self).__init__()
+
+        lambd = 1.5
+        gamma = 1
+        temp = round(abs((math.log2(inp) - gamma) / lambd))
+        kernel = temp if temp % 2 else temp - 1
+
+        self.h_cw = MD(3)
+        self.w_hc = MD(3)
+        self.no_spatial = no_spatial
+        if not no_spatial:
+            self.c_hw = MCAGate(kernel)
+
+    def forward(self, x):
+        x_h = x.permute(0, 2, 1, 3).contiguous()
+        x_h = self.h_cw(x_h)
+        x_h = x_h.permute(0, 2, 1, 3).contiguous()
+
+        x_w = x.permute(0, 3, 2, 1).contiguous()
+        x_w = self.w_hc(x_w)
+        x_w = x_w.permute(0, 3, 2, 1).contiguous()
+
+        if not self.no_spatial:
+            x_c = self.c_hw(x)
+            x_out = 1 / 3 * (x_c + x_h + x_w)
+        else:
+            x_out = 1 / 2 * (x_h + x_w)
+
+        return x_out
+
+
+class StemLayer(nn.Module):
+    def __init__(self,
+                 in_channels=3,
+                 out_channels=96,
+                 act_layer=nn.GELU,
+                 norm_layer=partial(nn.LayerNorm, eps=1e-6)):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels,
+                               out_channels // 2,
+                               kernel_size=3,
+                               stride=2,
+                               padding=1)
+        self.norm1 = norm_layer(out_channels // 2)
+        self.act = act_layer()
+        self.conv2 = nn.Conv2d(out_channels // 2,
+                               out_channels,
+                               kernel_size=3,
+                               stride=2,
+                               padding=1)
+        self.norm2 = norm_layer(out_channels)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = x.permute(0, 2, 3, 1)
+        x = self.norm1(x)
+        x = x.permute(0, 3, 1, 2)
+        x = self.act(x)
+        x = self.conv2(x)
+        x = x.permute(0, 2, 3, 1)
+        x = self.norm2(x)
+        return x
+
+
+class DownsampleLayer(nn.Module):
+    def __init__(self, in_channels=96, out_channels=198, norm_layer=partial(nn.LayerNorm, eps=1e-6)):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels,
+                              out_channels,
+                              kernel_size=3,
+                              stride=2,
+                              padding=1)
+        self.norm = norm_layer(out_channels)
+
+    def forward(self, x):
+        x = self.conv(x.permute(0, 3, 1, 2)).permute(0, 2, 3, 1)
+        x = self.norm(x)
+        return x
+
+
+class GatedCNNBlock(nn.Module):
+    def __init__(self, dim, expension_ratio=8/3, kernel_size=7, conv_ratio=1.0,
+                 norm_layer=partial(nn.LayerNorm,eps=1e-6),
+                 act_layer=nn.GELU,
+                 drop_path=0.,
+                 **kwargs):
+        super().__init__()
+        self.norm = norm_layer(dim)
+        hidden = int(expension_ratio * dim)
+        self.fc1 = nn.Linear(dim, hidden * 2)
+        self.act = act_layer()
+        conv_channels = int(conv_ratio * dim)
+        self.split_indices = (hidden, hidden - conv_channels, conv_channels)
+        self.conv = nn.Conv2d(conv_channels, conv_channels, kernel_size=kernel_size, padding=kernel_size//2, groups=conv_channels)
+        self.fc2 = nn.Linear(hidden, dim)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+
+    def forward(self, x):
+        shortcut = x # [B, H, W, C]
+        x = self.norm(x)
+        g, i, c = torch.split(self.fc1(x), self.split_indices, dim=-1)
+        c = c.permute(0, 3, 1, 2) # [B, H, W, C] -> [B, C, H, W]
+        c = self.conv(c)
+        c = c.permute(0, 2, 3, 1) # [B, C, H, W] -> [B, H, W, C]
+        x = self.fc2(self.act(g) * torch.cat((i, c), dim=-1))
+        x = self.drop_path(x)
+        return x + shortcut
+
+DOWNSAMPLE_LAYERS_FOUR_STAGES = [StemLayer] + [DownsampleLayer]*3
+
+
+class JFE(nn.Module):
+    def __init__(self, in_chans=3, num_classes=1000,
+                 depths=[3, 3, 9, 3],
+                 dims=[96, 192, 384, 576],
+                 downsample_layers=DOWNSAMPLE_LAYERS_FOUR_STAGES,
+                 norm_layer=partial(nn.LayerNorm, eps=1e-6),
+                 act_layer=nn.GELU,
+                 conv_ratio=1.0,
+                 kernel_size=7,
+                 drop_path_rate=0.,
+                 output_norm=partial(nn.LayerNorm, eps=1e-6),
+                 head_dropout=0.0,
+                 **kwargs,
+                 ):
+        super().__init__()
+        self.num_classes = num_classes
+
+        if not isinstance(depths, (list, tuple)):
+            depths = [depths] # it means the model has only one stage
+        if not isinstance(dims, (list, tuple)):
+            dims = [dims]
+
+        num_stage = len(depths)
+        self.num_stage = 2
+
+        if not isinstance(downsample_layers, (list, tuple)):
+            downsample_layers = [downsample_layers] * num_stage
+        down_dims = [in_chans] + dims
+        self.downsample_layers = nn.ModuleList(
+            [downsample_layers[i](down_dims[i], down_dims[i+1]) for i in range(num_stage)]
+        )
+
+        dp_rates=[x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))]
+
+        self.stages = nn.ModuleList()
+        cur = 0
+        for i in range(num_stage):
+            stage = nn.Sequential(
+                *[GatedCNNBlock(dim=dims[i],
+                norm_layer=norm_layer,
+                act_layer=act_layer,
+                kernel_size=kernel_size,
+                conv_ratio=conv_ratio,
+                drop_path=dp_rates[cur + j],
+                ) for j in range(depths[i])]
+            )
+            self.stages.append(stage)
+            cur += depths[i]
+
+        self.norm = output_norm(dims[-1])
+
+        self.apply(self._init_weights)
+        self.attention = [MCALayer(dims[i]) for i in range(num_stage)]
+
+    def _init_weights(self, m):
+        if isinstance(m, (nn.Conv2d, nn.Linear)):
+            trunc_normal_(m.weight, std=.02)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+
+    @torch.jit.ignore
+    def no_weight_decay(self):
+        return {'norm'}
+
+    def forward_features(self, x):
+        for i in range(self.num_stage):
+            x = self.downsample_layers[i](x)
+            x = self.stages[i](x)
+            x = self.attention[i](x)
+        return rearrange(x, 'b h w c -> b c h w')
+
+    def forward(self, x):
+        x = self.forward_features(x)
+        return x
